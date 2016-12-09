a6f825ae31963ac3cedb1a61a7afb70d345e21f9
Ivan Pik <contact@ivanpik.co.uk>
2016-12-09 13:53:06 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,5 +10,5 @@ app.get('/', function (req, res) {
 })
 
 app.listen(80, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('Example app listening on port 80!');
 })
