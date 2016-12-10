e05c346e9e8bb165957a44052dc804e97cbd1463
Ivan Pik <contact@ivanpik.co.uk>
2016-12-10 18:13:08 +0000

    Connect to MongoDB, add Gulp with SCSS compiling, include Bootstrap for quick prototyping

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('sass', function() {
+  return sass('src/scss/stylesheet.scss')
+    .pipe(gulp.dest('src/public/css'))
+    .pipe(reload({ stream:true }));
+});
+
+// watch Sass files for changes, run the Sass preprocessor with the 'sass' task and reload
+gulp.task('serve', ['sass'], function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  });
+
+  gulp.watch('src/scss/*.scss', ['sass']);
+});
--- src/app.js
+++ src/app.js
@@ -1,18 +1,99 @@
 'use strict';
 
-var settings = require('./settings.json')
+// ---------------------------------------------
+// IMPORT SETTINGS
+// environment settings
+var envSettings = require('./envSettings.json');
+// app settings
+var settings = require('./settings.json');
+
+// ---------------------------------------------
+// MODULES
 var express = require('express');
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer(); // for parsing multipart/form-data
 
-app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+// ---------------------------------------------
+// GLOBALS
+var db;
+
+// ---------------------------------------------
+// DB CONNECT
+
+MongoClient.connect(envSettings.db.uri, (err, database) => {
+  db = database;
+  listen();
+})
+
+// ---------------------------------------------
+// STATIC FILES
+app.use('/static', express.static(__dirname + '/public'));
+
+// ---------------------------------------------
+// VIEW ENGINE
 app.set('view engine', 'pug');
+app.set('views', __dirname  + '/views');
+
+// ---------------------------------------------
+// ROUTES
+
 
 app.get('/', function (req, res) {
-  let title = settings.frontend.appName + ' | ' + settings.frontend.appNote;
-  res.render(__dirname + '/views/layout', {"greeting": "Hello!", "title": title});
+  var title = settings.frontend.appName + ' | ' + settings.frontend.appNote;
+  res.locals.title = title;
+
+  db.collection('greetings').find().limit(1).sort({$natural:-1}).toArray((err, result) => {
+    if (err) {
+      return console.log(err);
+    }
+
+    // Pick a random greeting
+    var pickGreeting = (function (result) {
+      var count = result.length;
+      var key = Math.floor(Math.random() * count);
+      res.locals.greeting = result[key].content;
+    })(result);
+
+
+    res.render('home');
+  });
+});
+
+app.get('/thanks', function (req, res) {
+  var title = settings.frontend.appName + ' | ' + settings.frontend.appNote;
+  res.locals.title = title;
+  res.render('thanks');
+});
+
+
+app.post('/add-greeting', (req, res) => {
+  console.log("something is coming in!");
+  console.log(req.body);
+
+  var newGreeting = req.body.greeting;
+  var password = req.body.password;
+
+  if (password == envSettings.dummyUserPassword) {
+    db.collection('greetings').insert( { content: newGreeting } );
+    res.redirect('/thanks');
+  } else {
+    res.sendStatus(403);
+  }
+
+
 })
 
-app.listen(80, function () {
-  console.log('Example app listening on port 80!');
-})
+// ---------------------------------------------
+// LISTEN
+
+var listen = function() {
+  app.listen(80, function () {
+    console.log('Example app listening on port 80!');
+  });
+}
