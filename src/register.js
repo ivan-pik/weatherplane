32284e948d87ff3a213266a2838c2cc0d2c2b0c4
Ivan Pik <contact@ivanpik.co.uk>
2016-12-17 23:13:02 +0000

    Fix error where user would be redirected to “user/<id>” instead of “user/<userName>”

--- src/register.js
+++ src/register.js
@@ -44,7 +44,7 @@ router.post('/', function (req, res, next) {
         } else {
           req.session.userId = user._id;
           req.session.userSlug = user._userID;
-          res.redirect('/user/'+req.session.userId);
+          res.redirect('/user/'+req.session.userSlug);
         }
       })
 
