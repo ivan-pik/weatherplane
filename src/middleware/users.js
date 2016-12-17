c6c7651835f9024c09c9f40d4b9fc1fc7740b7a3
Ivan Pik <contact@ivanpik.co.uk>
2016-12-17 22:55:36 +0000

    Handle the case when logged-in user access profile page of another user

--- src/middleware/users.js
+++ src/middleware/users.js
@@ -9,6 +9,11 @@ function loggedOut (req, res, next) {
 
 function requiresLogin (req, res, next) {
   if (req.session && req.session.userId) {
+    if (req.params.userId != req.session.userSlug) {
+      var err = new Error("This is not your profile");
+      err.status = 403;
+      return next(err);
+    }
     return next();
   } else {
     var err = new Error("not logged in!!!");
