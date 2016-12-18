f4cc91d8a5297c275558efffe7d4e4612d2ae00c
Ivan Pik <contact@ivanpik.co.uk>
2016-12-18 23:01:22 +0000

    Added Users middleware comments

--- src/middleware/users.js
+++ src/middleware/users.js
@@ -1,11 +1,27 @@
 'use strict';
 
+// ---------------------------------------------
+// loggedOut
+/*
+When user is logged-in, redirect to profile page
+otherwise call next.
+Typically used so that logged-in user doesn't see login
+or register pages
+*/
 function loggedOut (req, res, next) {
   if (req.session && req.session.userId) {
     return res.redirect('/user/'+req.session.userSlug);
   }
   return next();
 }
+
+// ---------------------------------------------
+// requiresLogin
+/*
+When unaunthenticated user is acessing unauthorised pages
+redirect to login form. When authenticated user is accessing other
+user's profile, show error.
+*/
 
 function requiresLogin (req, res, next) {
   if (req.session && req.session.userId) {
