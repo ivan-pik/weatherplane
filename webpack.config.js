073df1d4c825caf054889b65ee69e780226f4ae9
Ivan Pik <ivanpik@ivan-pik-mbp.local>
2017-03-09 07:50:55 +0000

    Temporary webpack/vuejs stuff - This will be moved away to separate front-end app

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var path = require('path')
 const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -10,11 +11,27 @@ const config = {
     "./src/scss/main.scss"
   ],
   output: {
-    filename: "./src/public/js/scripts.js"
+    path: path.resolve(__dirname, './public/js/'),
+    publicPath: '/public/',
+    filename: "scripts.js"
   },
   module: {
     rules:
       [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+          options: {
+            loaders: {
+              // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+              // the "scss" and "sass" values for the lang attribute to the right configs here.
+              // other preprocessors should work out of the box, no loader config like this necessary.
+              'scss': 'vue-style-loader!css-loader!sass-loader',
+              'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+            }
+            // other vue-loader options go here
+          }
+        },
         {
           test: /\.scss$/,
           use: ExtractTextPlugin.extract({
@@ -35,15 +52,29 @@ const config = {
          }
        },
        {
-          test: /\.vue$/,
-          use: 'vue-loader',
-        },
+         test: /\.(png|jpg|gif|svg)$/,
+         loader: 'file-loader',
+         options: {
+           name: '[name].[ext]?[hash]'
+         }
+       }
       ]
 
   },
   resolve: {
-   extensions: ['*', '.js', '.es6']
+   extensions: ['*', '.js', '.es6'],
+   alias: {
+     'vue$': 'vue/dist/vue.esm.js'
+   }
   },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map',
   plugins: [
     new ExtractTextPlugin("./src/public/css/styles.css"),
     new webpack.LoaderOptionsPlugin({
@@ -58,5 +89,28 @@ const config = {
   ],
   watch: true
 }
+
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
+
 
 module.exports = config;
